f97ad970301c3840003f5b4ac07cba00903f0870
satesate <albi.c@msn.com>
2020-03-26 15:01:25 +0100

    .

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,20 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
-outputResult(currentResult, '');
+let calculationDescription = `(${defaultResult} +  10) * 3 / 2 - 1`;
+
+outputResult(currentResult, calculationDescription);
+
+let userInut = 20;
+let result;
+
+result = userInut + 18;
+
+result = result - 10;
+result = result * 2;
+result = result / 4;
+
+alert(userInut);
+alert(result);
